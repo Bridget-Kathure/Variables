a4ef1fe450f247abe49df75b6b414df7a3e532e0
Bridget-Kathure <bridgetkathuretitus@gmail.com>
2024-02-09 09:07:43 +0300

    first variable

---
+++ src/main/kotlin/Bridget.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var name = "Bridget Kathure"
+    var age = 22
+    var number = "0712345678"
+    var weight = 65
+    var kenyan = true
+    println(name)
+    println(age)
+    println(number)
+    println(weight)
+    println(kenyan)
+
+
+
+}
